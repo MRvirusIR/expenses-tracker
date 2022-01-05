3d1edd617bc809e6cbf57564501d1de3ccec8e68
г-н.вирус <a.h104110@gmail.com>
2022-01-05 15:32:45 +0330

    'new'

---
+++ src/api.py
@@ -0,0 +1,46 @@
+import sqlite3 as db
+from datetime import datetime
+
+con = db.connect('spent.db')
+cur = con.cursor()
+
+def init():
+    cur.execute('''CREATE TABLE IF NOT EXISTS expenses (
+        amount REAL,
+        category TEXT COLLATE NOCASE,
+        message TEXT,
+        date TEXT
+        )''')
+    con.commit()
+    con.close()
+    
+def add(amount, category, message = ''):
+    date = str(datetime.now().strftime('%Y - %m - %d | %H:%M'))
+    cur.execute('INSERT INTO expenses VALUES (:amount, :category, :message, :date)', {'amount': amount, 'category': category, 'message': message, 'date': date})
+    con.commit()
+    con.close()
+    
+def show(category=None):
+    if category:
+        cur.execute('SELECT * FROM expenses WHERE category = (:category)', {'category' : category})
+        results = cur.fetchall()
+        cur.execute('SELECT SUM(amount) FROM expenses WHERE category = (:category)', {'category' : category})
+        total_amount = cur.fetchone()[0]
+        
+    else:
+        cur.execute('''SELECT * FROM expenses''')
+        results = cur.fetchall()
+        cur.execute('''SELECT SUM(amount) FROM expenses''')
+        total_amount = cur.fetchone()[0]
+        
+    return total_amount,results
+    con.close()
+    
+def remove(category):
+    cur.execute('DELETE FROM expenses WHERE category = (:category)', {'category' : category})
+    con.commit()
+    con.close()
+
+# init()
+# add(100, 'coffee', 'ba bache ha coffee zadim eshg kardim')
+# print(show('coffee'))
---
+++ src/spent_app.py
@@ -0,0 +1,50 @@
+from docopt import docopt
+from api import *
+from tabulate import tabulate
+from colorama import Fore, Style, Back
+
+
+usage = '''
+Usage:
+    spent_app.p --init
+    spent_app.p --show [<category>]
+    spent_app.p --add <amount> <category> [<message>]
+    spent_app.p --remove <category>
+    spent_app.py -h | --help
+    
+Options:
+    --init          Initialize the database
+    --show          Show all expenses or expenses of a specific category
+    --add           Add an expense
+    --remove        Remove an expense
+    --version       Show version
+
+'''
+
+
+args = docopt(usage, options_first = True , version='spent_app.py 1.0', help=True)
+
+if args['--init']:
+    init()
+    print(f'{Back.LIGHTGREEN_EX}{Fore.BLACK}Successfully{Back.RESET}{Fore.RESET}')
+    
+if args['--show']:
+    category = args['<category>']
+    amount, results = show(category)
+    print('Total amount:', amount)
+    print(tabulate(results))
+    
+if args['--add']:
+    try:
+        amount = float(args['<amount>'])  
+        add(amount , args['<category>'] , args['<message>']) 
+        print(f'{Back.LIGHTGREEN_EX}{Fore.BLACK}Item added {Back.RESET}{Fore.RESET}')
+    except:
+        print(usage, f'{Fore.RESET}')    
+        
+if args['--remove']:
+    try:
+        remove(args['<category>'])
+        print(f'{Back.LIGHTGREEN_EX}{Fore.BLACK}Item removed {Back.RESET}{Fore.RESET}')
+    except:
+        print(usage, f'{Fore.RESET}')
